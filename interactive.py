7b29c1c23cabc6c31db18e28c2a0f931ec8d6452
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2021-01-09 19:04:09 +0100

    I forgot what that was

--- interactive.py
+++ interactive.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 from matplotlib.widgets import Slider
 
+NUM_DOF = 12
 
 def get_heatmap(phis):
     with subprocess.Popen(["./main"], stdin=subprocess.PIPE, stdout=subprocess.PIPE) as process:
@@ -18,9 +19,9 @@ def get_heatmap(phis):
 slidercolor = "lightgoldenrodyellow"
 fig = plt.figure()
 ax = plt.axes(xlim=(0, 256), ylim=(0, 256))
-im = plt.imshow(get_heatmap([0]*15), cmap="seismic", interpolation="none")
+im = plt.imshow(get_heatmap([0]*NUM_DOF), cmap="seismic", interpolation="none")
 
-slideraxes = [plt.axes([0.025, 0.1 + 0.05*i, 0.65, 0.03], facecolor=slidercolor) for i in range(15)]
+slideraxes = [plt.axes([0.025, 0.1 + 0.05*i, 0.65, 0.03], facecolor=slidercolor) for i in range(NUM_DOF)]
 sliders = [Slider(a, f"$\phi_{i}$", 0, 2*np.pi, valinit=0) for i,a in enumerate(slideraxes)]
 
 def update(val):
--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
+NUM_DOF = 12
+
 
 def get_heatmap(phis):
-    with subprocess.Popen(["./main"], stdin=subprocess.PIPE, stdout=subprocess.PIPE) as process:
+    with subprocess.Popen(["./builddir/main"], stdin=subprocess.PIPE, stdout=subprocess.PIPE) as process:
         out, errs = process.communicate(input=("\n".join((str(i) for i in phis)) + "\n").encode("UTF-8"))
 
     s = io.StringIO(out.decode("UTF-8"))
@@ -16,12 +18,10 @@ def get_heatmap(phis):
 
 
 #get_heatmap([ 5.29683196,  5.38560137,  3.29346721, -5.52313328, -4.40234988,
-#        1.95117847,  1.61550749,  4.63700674,  5.15551132,  0.63872836,
-#        1.05582973, -0.45151964, -2.93235769,  0.30883892,  2.13654636])
+#        1.95117847,  1.6NUM_DOF50749,  4.63700674,  5.15551132,  0.63872836,
+#        1.05582973, -0.45NUM_DOF1964, -2.93235769,  0.30883892,  2.13654636])
 
-d0 = [ 0,  0,  3.29346721, -5.52313328, -4.40234988,
-        1.95117847,  1.61550749,  4.63700674,  5.15551132,  0.63872836,
-        1.05582973, -0.45151964, -2.93235769,  0.30883892,  2.13654636]
+d0 = [0] * NUM_DOF
 
 fig = plt.figure()
 ax = plt.axes(xlim=(0, 256), ylim=(0, 256))
@@ -38,7 +38,7 @@ def animate(i):
     #a[0] = (i * 2*np.pi) / nsteps
     #a[1] = (i * 2*np.pi) / nsteps
     #print(a)
-    a = [(i * 2*np.pi) / nsteps] * 15
+    a = [(i * 2*np.pi) / nsteps] * NUM_DOF
     print(f"{i * 100 / nsteps} %")
     im.set_array(get_heatmap(a))
     return [im]
